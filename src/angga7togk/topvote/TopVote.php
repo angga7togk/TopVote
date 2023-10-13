f05541e43c3166b304c461547a91cd59930d3344
AraaCuteUwU <raihanxstorm14@gmail.com>
2023-10-13 17:05:23 +0800

    Clean code

--- src/angga7togk/topvote/TopVote.php
+++ src/angga7togk/topvote/TopVote.php
@@ -7,82 +7,86 @@ use pocketmine\command\CommandSender;
 use pocketmine\math\Vector3;
 use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
-use pocketmine\scheduler\ClosureTask;
 use pocketmine\utils\Config;
+use pocketmine\utils\SingletonTrait;
 
-class TopVote extends PluginBase {
+class TopVote extends PluginBase
+{
+    use SingletonTrait;
 
-	public Config $cfg;
-	public Config $pos;
-	private $particle = [];
-	public array $votes = [];
+    public Config $pos;
+    private array $particle = [];
+    public array $votes = [];
 
-	public static $instance;
-	
-	public function onLoad(): void {
-		self::$instance = $this;
-	}
+    public function onLoad(): void
+    {
+        self::setInstance($this);
+    }
 
-	public function onEnable():void{
-		$this->saveResource("config.yml");
-		$this->saveResource("pos.yml");
-		$this->cfg = new Config($this->getDataFolder(). "config.yml", Config::YAML, array());
-		$this->pos = new Config($this->getDataFolder(). "pos.yml", Config::YAML, array());
-		if(empty($this->cfg->get("key"))){
-			$this->getServer()->getLogger()->Info("Key Api vote not found!");
-			return;
-		}
-		if(!$this->pos->exists("position")){
-			$this->getServer()->getLogger()->Info("Please Set Location");
-			return;
-		}
-		$pos = $this->pos->get("position");
-		$this->particle[] = new FloatingText(new Vector3($pos[0], $pos[1], $pos[2]));
-		$this->getScheduler()->scheduleRepeatingTask(new UpdateTask($this), 20*5);
-    	$this->getServer()->getLogger()->Info("Location Have Been Load");
-	}
+    public function onEnable(): void
+    {
+        $this->saveResource("pos.yml");
+        $this->pos = new Config($this->getDataFolder() . "pos.yml", Config::YAML, array());
+        if (empty($this->getConfig()->get("key"))) {
+            $this->getServer()->getLogger()->warning("Key Api vote not found!");
+            $this->getServer()->shutdown(); //Stop the server if api key is empty
+            return;
+        }
+        if (!$this->pos->exists("position")) {
+            $this->getServer()->getLogger()->info("Please Set Location");
+            return;
+        }
+        $pos = $this->pos->get("position");
+        $this->particle[] = new FloatingText(new Vector3($pos[0], $pos[1], $pos[2]));
+        $this->getScheduler()->scheduleRepeatingTask(new UpdateTask($this), 20 * 5);
+        $this->getServer()->getLogger()->info("Location Have Been Load");
+    }
 
-	public function onCommand(CommandSender $sender, Command $command, String $label, array $args):bool{
-		if(!$sender instanceof Player){
-			$sender->sendMessage("pake command di server jir :V");
-			return false;
-		}
-		if($command->getName() == "settopvote"){
-			$this->pos->setNested("position", [$sender->getPosition()->getX(), $sender->getPosition()->getY(), $sender->getPosition()->getZ()]);
-			$this->pos->save();
-			$sender->sendMessage("Silakan restart server ya tod biar update");
-		}
-		return true;
-	}
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool
+    {
+        if (!$sender instanceof Player) {
+            $sender->sendMessage("pake command di server jir :V");
+            return false;
+        }
+        if ($command->getName() == "settopvote") {
+            $this->pos->setNested("position", [$sender->getPosition()->getX(), $sender->getPosition()->getY(), $sender->getPosition()->getZ()]);
+            $this->pos->save();
+            $sender->sendMessage("Silakan restart server ya tod biar update");
+        }
+        return true;
+    }
 
-	public function getLeaderBoard(): string{
-		$data = $this->votes;
-		$message = "";
-		$top = $this->cfg->get("format")["title"];
-		$footer = $this->cfg->get("format")["footer"];
-		if(count($data) > 0){
-    		arsort($data);
-    		$i = 1;
-			foreach ($data as $name => $vote) {
-				$message .= str_replace(["{top}", "{player}", "{vote}"], [$i, $name, $vote], $this->cfg->get("format")["message"]);
-				if($i > $this->cfg->get("top") - 1){
-				break;
-				}
-				++$i;
-			}
-		}
-    	return (string) $top.$message.$footer;
+    public function getLeaderBoard(): string
+    {
+        $data = $this->votes;
+        $message = "";
+        $top = $this->getConfig()->get("format")["title"];
+        $footer = $this->getConfig()->get("format")["footer"];
+        if (count($data) > 0) {
+            arsort($data);
+            $i = 1;
+            foreach ($data as $name => $vote) {
+                $message .= str_replace(["{top}", "{player}", "{vote}"], [$i, $name, $vote], $this->getConfig()->get("format")["message"]);
+                if ($i > $this->getConfig()->get("top") - 1) {
+                    break;
+                }
+                ++$i;
+            }
+        }
+        return $top . $message . $footer;
 
-	}
+    }
 
-	
-	public function getParticles(): array{
 
-		return $this->particle;
+    public function getParticles(): array
+    {
 
-	}
-	
-	public static function getInstance():self{
-		return self::$instance;
-	}
+        return $this->particle;
+
+    }
+
+    public static function getInstance(): self
+    {
+        return self::$instance;
+    }
 }
